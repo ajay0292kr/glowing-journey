a1911ecfa4f2f7b0130c0f3a32b1c415e587bb05
Ajay <ajay0292kr@yahoo.com>
2016-07-19 20:34:50 +0530

    Initial commit

---
+++ Array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Array {
+	public static void main(String[] args) {
+        int i,n;
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter the value of n:");
+        n=in.nextInt();
+        int [] a = new int[n];
+        System.out.println("please enter number:");
+        for( i=0;i<n;i++)
+         {
+    	  
+    	   a[i] = in.nextInt();
+         }
+        for(i=0;i<n;i++)
+         {
+    	   System.out.println(a[i]);
+         }
+
+	}
+
+}
